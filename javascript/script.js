d82a85809da40e2ee33c219abd0a76e5588b27de
federico bitsch <fedegamer11@gmail.com>
2024-02-09 03:17:40 -0300

    Agregando scripts

--- javascript/script.js
+++ javascript/script.js
@@ -31,3 +31,21 @@ function prevSlide(event) {
   updateCarousel();
 }
 
+
+function validarFormulario() {
+  // Obtener valores de los campos
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var telefono = document.getElementById('telefono').value;
+  var email = document.getElementById('email').value;
+
+  // Verificar campos obligatorios
+  if (nombre === '' || apellido === '' || telefono === '' || email === '') {
+    alert('Por favor, complete todos los campos obligatorios.');
+    return false; // Evitar el envío del formulario si hay campos vacíos
+  }
+
+  // Si todos los campos están completos, se puede enviar el formulario
+  return true;
+}
+
